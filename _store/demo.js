e08995fa4d1578adf12efe9b7042014da29bee32
zhoucongmin <1641626308@qq.com>
2017-03-31 17:44:43 +0800

    sdklfjsdfj

--- _store/demo.js
+++ _store/demo.js
@@ -1,22 +1,20 @@
 import alt from "../alt.js"
 
 import DemoActions from "../_action/demo.js";
-
 class DemoStore{
 	constructor() {
 		this.bindListeners({
 			updateTodo:DemoActions.updateTodo
 		})
-		this.state={
+		/*this.state={
 			todos:'465456'
-		}
+		}*/
+		this.todos='4654654654654';
 	}
 	updateTodo(todo){
 		console.log(todo,'store todo')
-		this.setState({
-			todos:todo[0]
-		})
+		this.todos=todo[0];
 	}
 }
 
-export default alt.createStore(DemoStore,'DemoStore')
+export default alt.createStore(DemoStore)
